e378a8daad804221a204eeade62d01e655fe073e
yuncity <yuncity@pku.edu.cn>
2017-12-21 21:14:45 +0800

    update

---
+++ app/src/main/java/com/cypyc/dorm_helper/activity/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.cypyc.dorm_helper.activity;
+
+/**
+ * Created by yuncity on 2017/11/11.
+ */
+
+public class LoginActivity {
+
+}
---
+++ app/src/main/java/com/cypyc/dorm_helper/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cypyc.dorm_helper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cypyc.dorm_helper.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
